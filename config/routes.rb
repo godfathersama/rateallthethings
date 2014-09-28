2e8792f05e230fb8378db6f2f2e83139fead7429
Michael Williams <mich@elwillia.ms>
2014-09-27 20:50:17 -0500

    Add welcome_controller and set root to welcome#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'welcome#index'
+
+  get 'welcome/contact'
+
+  get 'welcome/about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
